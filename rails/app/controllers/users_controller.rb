c62264bf273cbff3c289395a95170d4679c8f3fb
Davy Wentworth <davy.wentworth@gmail.com>
2014-05-09 23:15:08 -0700

    User creation/editing. Intitial user show page.

--- rails/app/controllers/users_controller.rb
+++ rails/app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_filter :find_user, except: :index
+  before_filter :find_user, only: [:edit, :show, :update]
   def index
     @users = User.all.order(:first_name)
   end
@@ -7,11 +7,38 @@ class UsersController < ApplicationController
   def show
   end
 
+  def new
+    @user = User.new
+    render "edit" and return
+  end
+
   def edit
+  end
+
+  def update
+    @user.update_attributes(user_params)
+    if @user.errors.present?
+      flash[:error] = @user.errors.messages
+      redirect_to edit_user_path() and return
+    end
+    redirect_to users_path
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.errors.present?
+      flash[:error] = @user.errors.messages
+      redirect_to new_user_path() and return
+    end
+    redirect_to users_path
   end
 
   private
     def find_user
       @user = User.find_by(id: params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
     end
 end
--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_and_belongs_to_many :songs
   has_many :events
-  
+
   validates :first_name, presence: true
   validates :email, presence: true, uniqueness: true
 end
