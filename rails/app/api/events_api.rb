81389d1a3f982d933c42a9875aadf8a6de851c93
darrenli <darren@thanx.com>
2014-05-09 22:50:45 -0700

    Remove tmp/* files and play random song.

--- rails/app/api/events_api.rb
+++ rails/app/api/events_api.rb
@@ -2,8 +2,12 @@ class EventsApi < Grape::API
   resource :events do
     post '/' do
       authenticate!
-
       @current_user.events.create( ibeacon_id: params[:ibeacon_id])
+      Music::Player.play('0890B76B47ADFFD8')
+      Music::Player.position = 45
+      sleep 10
+      Music::Player.stop
+      {}
     end
   end
 end
--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140509215107) do
+ActiveRecord::Schema.define(version: 20140510002234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140509215107) do
     t.integer  "ibeacon_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "songs", force: true do |t|
