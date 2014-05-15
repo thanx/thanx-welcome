b0573a884b77da4b0bd9cf6bf30f9e075cb9f807
darrenli <darren@thanx.com>
2014-05-15 02:06:28 -0700

    Install delayed job and play music asynchronously.

--- rails/app/models/event.rb
+++ rails/app/models/event.rb
@@ -1,3 +1,27 @@
 class Event < ActiveRecord::Base
   belongs_to :user
+
+  after_create :play_song
+
+  protected
+
+    def play_song
+      song = self.user.songs.offset(rand(self.user.songs.count)).first
+      if song.present?
+        Music::Player.pause
+        Music::Player.volume = 0
+        Music::Player.play(song.track_id)
+        Music::Player.position = song.start_at.to_f
+        # fade in
+
+        Music::Player.volume = 100
+        sleep (song.end_at - song.start_at)
+
+        # fade out
+        Music::Player.volume = 0
+        Music::Player.stop
+      end
+    end
+    handle_asynchronously :play_song
+
 end
