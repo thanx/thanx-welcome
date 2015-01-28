ec18ec5db6bb8af88ab5bbf0419e64f7e1ec1cf3
Darren Cheng <darren@thanx.com>
2015-01-28 02:12:08 -0800

    Pause spotify if necessary.

--- rails/app/models/song.rb
+++ rails/app/models/song.rb
@@ -6,14 +6,47 @@ class Song < ActiveRecord::Base
   validates_numericality_of :end_at, greater_than: :start_at
 
   def play
-    tunes = RubyTunes.new
-    tunes.playback.volume = 0
-    RubyTunes::Track.new(id: self.track_id).play
-    tunes.playback.position = self.start_at
-    tunes.fade.in
-    sleep(self.end_at - self.start_at)
-    tunes.fade.out
-    tunes.playback.pause
+    suppress_spotify do
+      tunes = RubyTunes.new
+      tunes.playback.volume = 0
+      RubyTunes::Track.new(id: self.track_id).play
+      tunes.playback.position = self.start_at
+      tunes.fade.in
+      sleep(self.end_at - self.start_at)
+      tunes.fade.out
+      tunes.playback.pause
+    end
+  end
+
+private
+
+  def suppress_spotify
+    spotify_is_playing = true if spotify? && spotify_state == 'playing'
+    pause_spotify if spotify_is_playing
+
+    yield
+
+    play_spotify if spotify_is_playing
+  end
+
+  def spotify_state
+    run('tell app "Spotify" to player state')
+  end
+
+  def pause_spotify
+    run('tell app "Spotify" to pause')
+  end
+
+  def play_spotify
+    run('tell app "Spotify" to play')
+  end
+
+  def spotify?
+    run('tell app "System Events" to count processes whose name is "Spotify"').to_i == 1
+  end
+
+  def run(command)
+    `osascript -e '#{command}'`.strip
   end
 
 end
